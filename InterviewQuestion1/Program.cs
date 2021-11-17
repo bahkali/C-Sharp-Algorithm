e63cf0969c9335b9350795b352ec446741180e95
kaly <kalimamadou@gmail.com>
2021-11-16 22:52:49 -0800

    C# algorigthm question 1: Brute force solution.

--- InterviewQuestion1/Program.cs
+++ InterviewQuestion1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,37 @@ namespace InterviewQuestion1
     {
         static void Main(string[] args)
         {
+
+            int[] arr = { 1, 3, 7, 9, 0 };
+            int target = 11;
+            Console.WriteLine("Question: Given an array of integers, return the indices of the two numbers that add up to a given target.");
+            int[] answeer = twoSum(arr, target);
+            if ( answeer == null) {
+                Console.WriteLine("Sorry return is null, Press any key to terminate.");
+                Console.ReadKey();
+                return; 
+            }
+            Array.ForEach(answeer, Console.WriteLine);
+
+            Console.ReadKey();
+        }
+
+        // BRUTE FORCE SOLUTION
+         static int[] twoSum(int[] arr, int target)
+        {
+            
+            for (int i = 0 ; i < arr.Length; i++)
+            { 
+                int numToFind = target - arr[i];
+                for (int j= i+1 ; j < arr.Length; j++)
+                {
+                    if (numToFind == arr[j])
+                    {
+                        return new int[] { i, j};
+                    }
+                }
+            }
+            return null;
         }
     }
 }
